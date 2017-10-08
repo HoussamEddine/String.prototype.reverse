571b59a21883791517ef0939734e9d4e78b2149a
Houssam-Eddine <chetouanihoussameddine@gmail.com>
2017-10-08 17:47:45 +0200

    adding the reverse method to the string function contructor

---
+++ String.prototype.reverse.js
@@ -0,0 +1,27 @@
+Object.defineProperty(String.prototype,'reverse',{
+	configurable : true , 
+	writable :true,
+	enumerable : false , 
+	value : function(start,end) {
+
+		var str = this ,
+		length = str.length ;
+
+		if(!start) {
+			start = 0 ;
+		} 
+		if(!end || end > length) {
+			end = length ;
+		}
+	
+					var newString = '' ;
+					
+						for(var i = end -1; i >= start; i--) {
+						newString +=str[i] ;
+						
+					}
+					
+						return newString ; 
+					}
+	
+})
